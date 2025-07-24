862963fa3152a07d9fe780ff286fd43bf43f289f
Ljupko Kolak <kljupko@gmail.com>
2025-07-24 23:42:11 +0200

    added chapter 1

---
+++ python_work/chapter_01/hello_world.py
@@ -0,0 +1,33 @@
+print("Hello Pyhton world!")
+
+
+
+
+
+# EXERCISE 1
+# Check out pyhon.org. No code required here.
+# -----------
+
+
+
+
+
+# EXERCISE 2
+# Make a typo that results in an error.
+# Make a typo that does not result in an error, and figure out why.
+# ------------
+
+# typo without error, as the error is not in the keyword, but the displayed text
+print("Hello Python wordl!")
+
+# typo with error, because the keyword is misspelled
+prit("Hello Python world!")
+
+
+
+
+
+# EXERCISE 3
+# Note down ideas for what you'd like to create when you develop your skills.
+# No code required here.
+# -----------
