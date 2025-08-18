52ed2165b1fd08773c09467275685fae420b3c97
Ljupko Kolak <kljupko@gmail.com>
2025-08-18 14:12:48 +0200

    finished chapter 18

--- projects/django/pizzeria_project/pizzas/urls.py
+++ projects/django/pizzeria_project/pizzas/urls.py
@@ -6,5 +6,9 @@ from . import views
 app_name = 'pizzas'
 urlpatterns = [
     # Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Page that shows all pizzas.
+    path('pizzas/', views.pizzas, name='pizzas'),
+    # Details about a pizza.
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'),
 ]
--- projects/django/pizzeria_project/pizzas/views.py
+++ projects/django/pizzeria_project/pizzas/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+from .models import Pizza
+
 # Create your views here.
 def index(request):
     """The home page for the pizzeria."""
     return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    """The page listing the pizzas."""
+    pizzas = Pizza.objects.order_by('name')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzas/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    """Shows info about a single pizza."""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('name')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzas/pizza.html', context)
